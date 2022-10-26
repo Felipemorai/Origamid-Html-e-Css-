bc2d54856707264454a01db87cea34d70f1b0cbc
Luiz Felipe <felipemorai188@gmail.com>
2022-10-26 11:41:57 -0300

    javascript

---
+++ Javascript/script.js
@@ -0,0 +1,9 @@
+/* Variáveis */
+/* Guardam um valor na mwmória e permitem a reutilização do mesmo. Três formas de declarar const, let, var. Usamos mais const e let. */
+const curso = 'JavaScript';
+
+let total = 10;
+total = 20;
+
+console.log(curso);
+console.log(total);
