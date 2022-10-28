0d184de89af36fee6479e810993e3bef40b76549
Luiz Felipe <felipemorai188@gmail.com>
2022-10-28 11:34:34 -0300

    string-number

---
+++ Javascript/tipos.js
@@ -0,0 +1,33 @@
+/* String */
+const text = 'JavaScript';
+const frase = 'Isso é o meu "jogo"';
+
+console.log(text);
+console.log(frase);
+console.log(text + ' ' + frase);
+
+/* Number */
+let total = 0;
+
+const compras = 30;
+const imposto = 10;
+
+total = compras + imposto;
+
+const dobro = total * 2;
+const dividir = total / 2;
+const desconto = total - 10;
+
+console.log(total);
+console.log(dobro);
+console.log(dividir);
+console.log(desconto);
+
+const strings = '20' + '20';
+const numbers = 20 + 20;
+
+const transformar = Number("20");
+
+console.log(strings);
+console.log(numbers);
+console.log(transformar);
